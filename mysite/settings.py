a93fc34eafeb3e7609d9f549fc24eed8753be5e9
Erik Khachatryan <khachatrian.erik@gmail.com>
2020-05-28 02:56:00 +0400

    initial commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-ALLOWED_HOSTS = ['khachatrianerik.pythonanywhere.com']
+ALLOWED_HOSTS = ['khachatrianerik.pythonanywhere.com', '127.0.0.1']
